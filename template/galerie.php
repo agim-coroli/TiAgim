b250c0b6c597306591bb74b668944f1e525786e0
agim <agim.coroli8016@gmail.com>
2025-02-19 12:16:29 +0100

    galerie end

--- template/galerie.php
+++ template/galerie.php
@@ -12,9 +12,34 @@
         <?php include ("inc/menu.php"); ?>
     </header>
     <main>
-    <div class="section">
-            <h1></h1>
-            <img src="img/" alt="">
+    <div class="section gal_section">
+            <h1>Galerie Photo</h1>
+            <div>
+                <div>
+                    <img src="img/480px_DublinTheCustomHouse-2014-10.jpg" alt="House custom" title="test">
+                    <p>House custom</p>
+                </div>
+                <div>
+                    <img src="img/480px_GeorgeSalmonTrinityCollegeDublin.jpg" alt="College dublin">
+                    <p>College Dublin</p>
+                </div>
+                <div>
+                    <img src="img/480px_La_cathédrale_Saint-Patrick.jpg" alt="Cathedral saint patrick">
+                    <p>Cathedral saint patrick</p>
+                </div>
+                <div>
+                    <img src="img/480px_O'Connell_Bridge.jpg" alt="Pont O'connell">
+                    <p>Pont O'connell</p>
+                </div>
+                <div>
+                    <img src="img/480px_OConnell_Street_Dublin.jpg" alt="Rue O'connell">
+                    <p>Rue O'connell</p>
+                </div>
+                <div>
+                    <img src="img/480px_Samuel_Beckett_Bridge.jpeg" alt="Pont Samuel Becket">
+                    <p>Pont Samuel Becket</p>
+                </div>
+            </div>
         </div>
     </main>
     <footer>
