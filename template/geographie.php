9707a1e3b539c3625efb5316bd80e02cb0be4772
agim <agim.coroli8016@gmail.com>
2025-02-19 11:05:53 +0100

    page accueil & géo end + css

--- template/geographie.php
+++ template/geographie.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title><?= $tilte ?></title>
 </head>
+
 <body>
-<header>
-        <?php include ("inc/banniere.php"); ?>
-        <?php include ("inc/menu.php"); ?>
+    <header>
+        <?php include("inc/banniere.php"); ?>
+        <?php include("inc/menu.php"); ?>
     </header>
     <main>
-
+        <div class="section geo_section">
+            <h1>Capitale multiple et centre diplomatique mondial</h1>
+            <img src="img/geographie.png" alt="Carte du l'irlande">
+            <p>La ville est située à peu près au milieu de la côte est de l'Irlande, le long d'une baie, à l'embouchure de la Liffey et au centre de la région du Grand Dublin. Au sud de la ville s'étendent les montagnes de Wicklow.</p>
+            <p>Son climat est typiquement océanique. On peut le comparer au climat de Vancouver. Les hivers sont doux (environ 5 °C) et les étés frais (environ 15 °C). Les précipitations, d'un total de 733 mm, sont bien réparties tout au long de l'année.</p>
+            <p>Les loyers réclamés aux locataires sont très élevés. En 2019, ils se situent entre 1 300 et 1 800 euros en moyenne pour un logement avec une chambre. Les prix ont augmenté en moyenne de 70 % entre 2012 et 2020, faisant de Dublin la capitale européenne la plus onéreuse pour le montant des loyers.</p>
+            <p>Il existe traditionnellement une opposition entre le nord et le sud de la ville, avec la ligne de démarcation formée par la Liffey. Le Northside est plus pauvre et ouvrier, tandis que le Southside est considéré comme plus aisé, occupé par les classes moyennes et supérieures. Cette division se retrouve dans les codes postaux attribués aux quartiers, le Nord ayant des numéros impairs tandis que les numéros pairs sont attribués aux quartiers Sud. Cette division date d'il y a des siècles, sans doute à l'époque où le comte de Kildare a construit sa résidence au Sud, à l'époque peu développé, et a été rapidement suivi par ses pairs ; quand on lui demandait pourquoi il allait s'installer au Sud, il répondait « Où je vais me suit la mode ».</p>
+            <p>En opposition, bien que le Sud soit plus aisé, la résidence officielle du président d'Irlande (Áras an Uachtaráin) se trouve au Nord (mais avec le code postal 8, normalement du Sud). La résidence de l'archevêque catholique de Dublin et de son homologue anglican jusqu'en 1920 sont elles aussi situées dans le Northside, ainsi que l'une des banlieues les plus riches de Dublin, la colline de Howth, qui est également localisée au Nord.</p>
+            <p>Il existe également de nombreuses banlieues ouvrières dans le Sud, comme Palmerstown, Crumlin et Ballyfermot.</p>
+            <p>Les classes moyennes de Dublin sont souvent appelées Dublin 4, en référence au code postal de l'un des quartiers les plus riches de Dublin, dans lequel se trouvent les studios de la radio nationale, Radio Telefís Éireann, ainsi que bon nombre d'écoles et de lycées réputés, une université et les ambassades. Le campus moderne de l'University College of Dublin se trouve à la limite entre Dublin 4 et Dublin 14.</p>
+            <p>En fait, le terme Dublin 4 ou son abréviation D4 peut s'appliquer à n'importe quel Dublinois de classe moyenne, aussi bien du Northside que du Southside, ou plus souvent à une attitude que l'on peut trouver partout ailleurs en Irlande. De nombreux politiciens et politologues vivent à Dublin 4, et ce quartier prend traditionnellement des positions très libérales lors des référendums sur des sujets tels que l'avortement ou le divorce. Dublin 4 est également associé à un certain accent (pas vraiment spécifique à ce quartier), que certains apprécient et d'autres abhorrent.</p>
+        </div>
     </main>
     <footer>
 
     </footer>
 </body>
+
 </html>
