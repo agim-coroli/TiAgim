fbe412a49109b16213cfe38fe53e9a5a6b5df630
agim <agim.coroli8016@gmail.com>
2025-02-19 12:56:38 +0100

    end error404

--- template/page-404.php
+++ template/page-404.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title><?= $tilte ?></title>
 </head>
+
 <body>
     <header>
-        <?php include ("inc/banniere.php"); ?>
-        <?php include ("inc/menu.php"); ?>
+        <?php include("inc/banniere.php"); ?>
+        <?php include("inc/menu.php"); ?>
     </header>
     <main>
-    <div class="section">
-            <h1></h1>
-            <img src="img/" alt="">
+        <div class="section error_section">
+            <h1>Erreur 404</h1>
+            <div>
+                <img src="img/404.jpg" alt="Photo d'erreur 404">
+            </div>
+            <p>Euh, la page demandée est introuvable !</p>
+            <p>Le mieux est de trtourner a l'accueil.</p>
         </div>
     </main>
     <footer>
 
     </footer>
 </body>
+
 </html>
