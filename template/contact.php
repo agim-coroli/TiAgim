b8de2344fa68b4be9eb5cd430a9110ddf751dd2a
agim <agim.coroli8016@gmail.com>
2025-02-19 12:50:34 +0100

    end contact & liens

--- template/contact.php
+++ template/contact.php
@@ -1,24 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title><?= $tilte ?></title>
 </head>
+
 <body>
-<header>
-        <?php include ("inc/banniere.php"); ?>
-        <?php include ("inc/menu.php"); ?>
+    <header>
+        <?php include("inc/banniere.php"); ?>
+        <?php include("inc/menu.php"); ?>
     </header>
     <main>
-    <div class="section">
-            <h1></h1>
-            <img src="img/" alt="">
+        <div class="section cont_section">
+            <h1>Pour nous contacter</h1>
+            <div>
+                <img src="img/contacts.jpg" alt="image d'enveloppe">
+            </div>
+            <p>Pour nous laisser un commentaire ou pour obtenir un renseignement complémentaire, remplissez le formulaire ci-dessous :</p>
+            <form action="" method="post">
+                <div>
+                    <label for="">NOM :</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">PRENOM :</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">VILLE :</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">E-MAIL :</label>
+                    <input type="email">
+                </div>
+                <div class="big_input">
+                    <label for="">COMMENTAIRES :</label>
+                    <input type="text">
+                </div>
+                <div class="btn">
+                    <input type="button" value="Envoyez votre message">
+                </div>
+            </form>
         </div>
     </main>
     <footer>
 
     </footer>
 </body>
+
 </html>
--- template/liens.php
+++ template/liens.php
@@ -14,9 +14,38 @@
         <?php include("inc/menu.php"); ?>
     </header>
     <main>
-    <div class="section">
-            <h1></h1>
-            <img src="img/" alt="">
+        <div class="section link_section">
+            <h1>Quelques liens vers nos partenaires.</h1>
+            <div>
+                <img src="img/liens.jpg" alt="">
+            </div>
+            <p>D'autres capitales se sont associées à notre site.</p>
+            <div class="list_container">
+                <ul>
+                    <li><a href="">Berlin</a></li>
+                    <li><a href="">Vienne</a></li>
+                    <li><a href="">Paris</a></li>
+                    <li><a href="">Madrid</a></li>
+                    <li><a href="">Rome</a></li>
+                    <li><a href="">Londres</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Lisbonne</a></li>
+                    <li><a href="">Prague</a></li>
+                    <li><a href="">Amsterdam</a></li>
+                    <li><a href="">Copenhague</a></li>
+                    <li><a href="">Dublin</a></li>
+                    <li><a href="">Athènes</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Sofia</a></li>
+                    <li><a href="">Varsovie</a></li>
+                    <li><a href="">Helsinki</a></li>
+                    <li><a href="">Stockholm</a></li>
+                    <li><a href="">Oslo</a></li>
+                    <li><a href="">Budapest</a></li>
+                </ul>
+            </div>
         </div>
     </main>
     <footer>
