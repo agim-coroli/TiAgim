4e709f78bc372df375380628153335eee71e37b5
agim <agim.coroli8016@gmail.com>
2025-02-19 09:44:41 +0100

    add style.css & titles

--- public/index.php
+++ public/index.php
@@ -2,27 +2,35 @@
 if (isset($_GET['p'])) {
     switch ($_GET['p']) {
         case 'geo':
-            include ("../template/");
+            include("../template/geographie");
+            $tilte = "geo";
             break;
         case 'hist':
-            include ("../template/geographie.php");
+            include("../template/histoire.php.php");
+            $tilte = "hist";
             break;
         case 'cult':
-            include ("../template/culture.php");
+            include("../template/culture.php");
+            $tilte = "cult";
             break;
         case 'gal':
-            include ("../template/galerie.php");
+            include("../template/galerie.php");
+            $tilte = "gal";
             break;
         case 'form':
-            include ("../template/contact.php");
+            include("../template/contact.php");
+            $tilte = "form";
             break;
         case 'link':
-            include ("../template/liens.php");
+            include("../template/liens.php");
+            $tilte = "link";
             break;
         default:
+            $tilte = "404";
             include("../template/page-404.php");
             break;
     }
 } else {
+    $tilte = "accueil";
     include("../template/accueil.php");
 }
